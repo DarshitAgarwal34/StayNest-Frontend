540c66087ad7355d47fe766e2249717a1b6ef6eb
DarshitAgarwal34 <agarwaldarshit680@gmail.com>
2026-05-20 16:11:23 +0530

    CC to Render

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss(),],
   server: {
     proxy: {
-      '/api': 'http://localhost:5000', // proxy to backend
+      '/api': 'https://staynest-backend-3vr8.onrender.com', // proxy to backend
     },
   },
 })
